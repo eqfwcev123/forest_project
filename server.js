ec15f5288d7fce3e8662cfc570a459b71af2aa66
eqfwcev123 <dohyeonee95@hotmail.com>
2019-11-21 13:59:43 +0900

    refactaored server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ const port = process.env.PORT || 5100;
 const router = require('./routes/login');
 app.use(cors());
 
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-// mongoose.connection.on("error", error => console.error(error));
-// mongoose.connection.once("open", () => console.log("connected to database"));
-
 // 미드웨어
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -22,8 +18,6 @@ app.use("/login", router);
 // nodejs 의 native promise 사용
 mongoose.Promise = global.Promise;
 mongoose.set("useFindAndModify", false);
-
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("Successfully connected to mongodb"))
